7ee3123b6824d8c555465139607b8793ad73fd1c
rionymous_ <callmerion24@gmail.com>
2019-11-17 16:49:52 +0700

    Initial commit

---
+++ app/src/main/java/com/androidrion/gradient/MainActivity.java
@@ -0,0 +1,22 @@
+package com.androidrion.gradient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.contraintLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
